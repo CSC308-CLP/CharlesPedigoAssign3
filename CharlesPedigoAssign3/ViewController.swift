e5aa7c8df78acf73c0c1cf1c5723d5a0f8bd5b97
Charles Lucas Pedigo <charles_pedigo@mymail.eku.edu>
2026-03-04 20:42:59 -0500

    Final Day GUI and element implementation. Coding still needs implementation

--- CharlesPedigoAssign3/ViewController.swift
+++ CharlesPedigoAssign3/ViewController.swift
@@ -9,9 +9,66 @@ import UIKit
 
 class ViewController: UIViewController {
 
+//    //Recipe Book Outlets/Actions
+//    //@IBAction func galbiButton(_ sender: Any) {
+//    //}
+//    
+//    //@IBAction func galbiButton(_ sender: UIButton) {
+//        //detailViewLabel.text = "Galbi"
+//        //detailViewImage.image = UIImage(named: "galbi")
+//        //detailViewDesc.text = "Galbi is a Korean dish consisting of thinly sliced marinated beef grilled to perfection."
+//    //}
+//    //@IBOutlet weak var galbiButtonOutlet: UIButton!
+//    
+//    
+//    @IBAction func pizzaButton(_ sender: UIButton) {
+//        detailViewLabel.text = "Pizza"
+//        detailViewImage.image = UIImage(named: "pizza")
+//        detailViewDesc.text = "Pizza is a popular dish of Italian origin, typically consisting of a flatbread topped with tomato sauce, mozzarella cheese, and various other ingredients such as pepperoni, mushrooms, bell peppers, onions, olives, and pine nuts."
+//    }
+//    @IBOutlet weak var pizzaButtonOutlet: UIButton!
+//    
+//    
+//    @IBAction func hamburgerButton(_ sender: UIButton) {
+//        detailViewLabel.text = "Hamburger"
+//        detailViewImage.image = UIImage(named: "hamburger")
+//        detailViewDesc.text = "A hamburger is a sandwich typically consisting of a round bun, filled with ground beef, cheese, and various other ingredients such as lettuce, tomato, onion, pickles, and condiments like ketchup and mustard."
+//    }
+//    @IBOutlet weak var hamburgerButtonOutlet: UIButton!
+//    
+//    
+//    @IBAction func reviewButton(_ sender: Any) {
+//    }
+//    
+//    @IBOutlet weak var ratingLabel: UILabel!
+//    
+//    //Detail View Controller Elements
+//    @IBOutlet weak var detailViewLabel: UILabel!
+//    @IBOutlet weak var detailViewImage: UIImageView!
+//    @IBOutlet weak var detailViewDesc: UILabel!
+//    
+//    //Review View Controller Elements
+//    
+//    @IBOutlet var starButtons: [UIButton]!
+//    
+//    @IBAction func starTapped(_ sender: UIButton) {
+//    }
+//    
+//    @IBOutlet weak var reviewRatingLabel: UILabel!
+//    
+//    @IBAction func reviewSubmitButton(_ sender: UIButton) {
+//    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //Round Recipe Book Menu Buttons
+        //galbiButtonOutlet.layer.cornerRadius = 20
+        //pizzaButtonOutlet.layer.cornerRadius = 20
+        //hamburgerButtonOutlet.layer.cornerRadius = 20
+        
     }
 
 
